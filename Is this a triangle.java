812d9642cef25a01f5ea60c5f798026d6eaf81ce
Mark <marikgames12@gmail.com>
2023-06-30 23:55:10 +0300

    7ky

---
+++ Is this a triangle.java
@@ -0,0 +1,8 @@
+class TriangleTester{
+  public static boolean isTriangle(int a, int b, int c){
+   if(a <= 0 || b <= 0 || c <= 0){
+     return false;
+   }
+    return a + b > c && b + c > a && c + a > b;
+  }
+}
